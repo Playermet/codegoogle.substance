394d2dcc73feb26c794c0547b6249aed20610b46
objeck <objeck@ddfe6f0e-f990-72b7-f321-d84324c9d8fa>
2013-06-08 18:08:54 +0000

    more code

---
+++ trunk/src/runtime/classes.cpp
@@ -0,0 +1,147 @@
+#include "classes.h"
+
+/****************************
+ * Integer class
+ ****************************/
+IntegerClass* IntegerClass::instance;
+
+IntegerClass::IntegerClass() {
+  AddOperation(L"+", Add);
+  AddOperation(L"*", Multiply);
+  AddOperation(L"<", Less);
+}
+
+IntegerClass::~IntegerClass() {
+}
+
+void IntegerClass::Add(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = INT_VALUE;
+    result->klass = right->klass;
+    result->value.int_value = left->value.int_value + right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->klass = right->klass;
+    result->value.float_value = left->value.int_value + right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid mathematical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
+
+void IntegerClass::Multiply(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = INT_VALUE;
+    result->klass = right->klass;
+    result->value.int_value = left->value.int_value * right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->klass = right->klass;
+    result->value.float_value = left->value.int_value * right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid mathematical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
+
+void IntegerClass::Less(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = BOOL_VALUE;
+    result->value.int_value = left->value.int_value < right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = BOOL_VALUE;
+    result->value.float_value = left->value.int_value < right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid logical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
+
+/****************************
+ * Float class
+ ****************************/
+FloatClass* FloatClass::instance;
+
+FloatClass::FloatClass() {
+  AddOperation(L"+", Add);
+  AddOperation(L"*", Multiply);
+  AddOperation(L"<", Less);
+}
+
+FloatClass::~FloatClass() {
+}
+
+void FloatClass::Add(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->value.float_value = left->value.float_value + right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->value.float_value = left->value.float_value + right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid mathematical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
+
+void FloatClass::Multiply(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->value.float_value = left->value.float_value * right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = FLOAT_VALUE;
+    result->value.float_value = left->value.float_value * right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid mathematical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
+
+void FloatClass::Less(Value* left, Value* right, Value* result) {
+  switch(right->type) {
+  case INT_VALUE:
+    result->type = BOOL_VALUE;
+    result->value.float_value = left->value.float_value < right->value.int_value;
+    break;
+
+  case FLOAT_VALUE:
+    result->type = BOOL_VALUE;
+    result->value.float_value = left->value.float_value < right->value.float_value;
+    break;
+
+  default:
+    wcerr << L">>> invalid mathematical operation <<<" << endl;
+    exit(1);
+    break;
+  }
+}
