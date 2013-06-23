de52fd7bb84dbb303f83c80f108eaee6ddd5962a
objeck@gmail.com <objeck@gmail.com@ddfe6f0e-f990-72b7-f321-d84324c9d8fa>
2013-06-23 18:48:55 +0000

    * adding support for additional operations * milestone will be creating a testbed that can debugged and optimized for supported platforms * platforms are: Windows 32/64, Linux 32/64 and OS X (64-bit) - required 3 different JIT compilers

--- trunk/src/classes.h
+++ trunk/src/classes.h
@@ -1,33 +1,33 @@
 /***************************************************************************
- * Base class for all runtime classes
- *
- * Copyright (c) 2013 Randy Hollines
- * All rights reserved.
- *
- * Redistribution and uses in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in
- * the documentation and/or other materials provided with the distribution.
- * - Neither the name of the Substance team nor the names of its
- * contributors may be used to endorse or promote products derived
- * from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ***************************************************************************/
+* Base class for all runtime classes
+*
+* Copyright (c) 2013 Randy Hollines
+* All rights reserved.
+*
+* Redistribution and uses in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in
+* the documentation and/or other materials provided with the distribution.
+* - Neither the name of the Substance team nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+*  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************/
 
 #ifndef __CLASS_H__
 #define __CLASS_H__
@@ -40,15 +40,15 @@ using namespace std;
 typedef void(*Operation)(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
 
 /****************************
- * Base class for built-in types
- ****************************/
+* Base class for built-in types
+****************************/
 class RuntimeClass {
   unordered_map<wstring, Operation> operations;
 
- protected:
+protected:
   RuntimeClass() {
   }
-  
+
   virtual ~RuntimeClass() {
   }
 
@@ -68,22 +68,26 @@ public:
 };
 
 /****************************
- * Integer class
- ****************************/
+* Integer class
+****************************/
 class IntegerClass : public RuntimeClass {
   static IntegerClass* instance;
-	
-	IntegerClass() {
-		AddOperation(L"+", Add);
-		AddOperation(L"*", Multiply);
-		AddOperation(L"<", Less);
-		AddOperation(L">", Greater);
-	}
 
-	~IntegerClass() {
-	}
-	
- public:
+  IntegerClass() {
+    AddOperation(L"+", Add);
+    AddOperation(L"-", Subtract);
+    AddOperation(L"*", Multiply);
+    AddOperation(L"/", Divide);
+    AddOperation(L"==", Equal);
+    AddOperation(L"!=", NotEqual);
+    AddOperation(L"<", Less);
+    AddOperation(L">", Greater);
+  }
+
+  ~IntegerClass() {
+  }
+
+public:
   static IntegerClass* Instance() {
     if(!instance) {
       instance = new IntegerClass;
@@ -92,37 +96,37 @@ class IntegerClass : public RuntimeClass {
     return instance;
   }
 
-  static void Add(Value &left, Value &right, Value &result, 
-									vector<jit::JitInstruction*> &jit_instrs, 
-									bool is_recording);
-  static void Multiply(Value &left, Value &right, Value &result, 
-											 vector<jit::JitInstruction*> &jit_instrs, 
-											 bool is_recording);
-  static void Less(Value &left, Value &right, Value &result, 
-									 vector<jit::JitInstruction*> &jit_instrs, 
-									 bool is_recording);
-	static void Greater(Value &left, Value &right, Value &result, 
-											vector<jit::JitInstruction*> &jit_instrs, 
-											bool is_recording);
+  static void Add(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Subtract(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Multiply(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Divide(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Equal(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void NotEqual(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Less(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Greater(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
 };
 
 /****************************
- * Float class
- ****************************/
+* Float class
+****************************/
 class FloatClass : public RuntimeClass {
   static FloatClass* instance;
-	
-	FloatClass() {
-		AddOperation(L"+", Add);
-		AddOperation(L"*", Multiply);
-		AddOperation(L"<", Less);
-		AddOperation(L">", Greater);
-	}
-	
-	~FloatClass() {
-	}
-	
- public:
+
+  FloatClass() {
+    AddOperation(L"+", Add);
+    AddOperation(L"-", Subtract);
+    AddOperation(L"*", Multiply);
+    AddOperation(L"/", Divide);
+    AddOperation(L"==", Equal);
+    AddOperation(L"!=", NotEqual);
+    AddOperation(L"<", Less);
+    AddOperation(L">", Greater);
+  }
+
+  ~FloatClass() {
+  }
+
+public:
   static FloatClass* Instance() {
     if(!instance) {
       instance = new FloatClass;
@@ -132,9 +136,13 @@ class FloatClass : public RuntimeClass {
   }
 
   static void Add(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Subtract(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
   static void Multiply(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Divide(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Equal(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void NotEqual(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
   static void Less(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
-	static void Greater(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
+  static void Greater(Value &left, Value &right, Value &result, vector<jit::JitInstruction*> &jit_instrs, bool is_recording);
 };
 
 #endif
