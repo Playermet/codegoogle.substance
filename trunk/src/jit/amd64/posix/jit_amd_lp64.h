23daeaa4e7750139c0cecccf2ac0c49034bba894
objeck@gmail.com <objeck@gmail.com@ddfe6f0e-f990-72b7-f321-d84324c9d8fa>
2013-06-24 18:43:23 +0000

    code clean up

--- trunk/src/jit/amd64/posix/jit_amd_lp64.h
+++ trunk/src/jit/amd64/posix/jit_amd_lp64.h
@@ -893,7 +893,9 @@ namespace jit {
 			aval_xregs.push_back(new RegisterHolder(XMM11));
 			aval_xregs.push_back(new RegisterHolder(XMM10));   
 #ifdef _DEBUG
-			wcout << L"Compiling code for AMD64 architecture..." << endl;
+			wcout << L"===========================================" << endl;
+      wcout << L"=== Compiling block for AMD64 (LP) host ===" << endl;
+      wcout << L"===========================================" << endl;
 #endif
 	
 			// process offsets
