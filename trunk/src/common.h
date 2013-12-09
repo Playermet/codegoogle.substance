b1c254ddd356592ef1be6f03141b585e488cca94
objeck@gmail.com <objeck@gmail.com@ddfe6f0e-f990-72b7-f321-d84324c9d8fa>
2013-12-09 21:17:19 +0000

    adding Xcode support

--- trunk/src/common.h
+++ trunk/src/common.h
@@ -53,12 +53,9 @@
 #include <unordered_map>
 using namespace stdext;
 #else
-#include <tr1/unordered_map>
+#include <unordered_map>
 #include <pthread.h>
 #include <stdint.h>
-namespace std {
-  using namespace tr1;
-}
 #endif
 
 using namespace std;
